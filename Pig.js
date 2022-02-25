0f17b856cdee5a11a5a05ed2502aebb0d643772d
LeandroSaramago <leandosaramago@gmail.com>
2022-02-25 15:54:13 -0300

    D

--- Pig.js
+++ Pig.js
@@ -9,11 +9,20 @@ class Pig {
       this.width = 70;
       this.height = 70;
       this.image = loadImage("enemy.png")
+      this.v = 255
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
       var angle =this.body.angle;
+      if(this.body.speed>3){
+        World.remove(world, this.body)
+        push();
+       this.v = this.v-5
+       tint(255, this.v)
+       image(this.image, this.body.position.x, this.body.position.y, 70, 70)
+       pop();
+      }else{
       push();
       translate(pos.x, pos.y)
       rotate(angle)
@@ -22,4 +31,5 @@ class Pig {
       image(this.image, 0, 0, this.width, this.height);
       pop();
     }
+  }
   };
